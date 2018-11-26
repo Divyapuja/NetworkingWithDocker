4a7ffd6f6573074f6cce98ce1b97f5955c79d4ae
Divyapuja <divyapuja.vitonde@gmail.com>
2018-11-25 19:35:33 -0500

    modified python and input

--- csv_to_yaml.py
+++ csv_to_yaml.py
@@ -1,21 +1,4 @@
 #!/usr/bin/env python
- 
-import csv
-import sys
-import yaml
- 
-csv_data = []
-with open(sys.argv[1]) as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        csv_data.append(row)
- 
-with open(sys.argv[1] + '.yml', 'w') as outfile:
-    outfile.write(yaml.dump({'csv_data': csv_data}))
-  
-  
-  /*--------------------------------------------*/
-  #!/usr/bin/env python
 
 import csv
 import sys
@@ -24,14 +7,10 @@ import yaml
 csv_data = []
 with open(sys.argv[1]) as csvfile:
     reader = csv.DictReader(csvfile)
+    print(reader[0])
     for row in reader:
-        print(row)
-        for line in row:
-            print(line)
-            line = line.replace(" ", "_")
-            row.append(line)
-#       trimmedRow = row.replace(" ", "_")
         csv_data.append(row)
 
 with open(sys.argv[1] + '.yml', 'w') as outfile:
     outfile.write(yaml.dump({'csv_data': csv_data}))
+
