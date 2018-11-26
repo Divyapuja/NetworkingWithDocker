64ed7fe785fe1bd0a8bd4bfdf29279d2bcec02b4
divyapuja <divyapuja.vitonde@gmail.com>
2018-11-25 22:20:14 -0500

    modifies python for desired output

--- csv_to_yaml.py
+++ csv_to_yaml.py
@@ -7,10 +7,19 @@ import yaml
 csv_data = []
 with open(sys.argv[1]) as csvfile:
     reader = csv.DictReader(csvfile)
-    print(reader[0])
     for row in reader:
         csv_data.append(row)
 
 with open(sys.argv[1] + '.yml', 'w') as outfile:
-    outfile.write(yaml.dump({'csv_data': csv_data}))
+    outfile.write(yaml.dump(csv_data))
 
+with open(sys.argv[1] + '.yml', 'rU') as f:
+    data = yaml.safe_load(f)
+    for line in data:
+        for i in line.keys():
+            NewKey = i.replace(" ", "_")
+	    line[NewKey] = line[i]
+            del line[i]
+
+with open(sys.argv[1] + '.yml', 'w') as f:
+    yaml.dump(data, f)
