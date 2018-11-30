316493abf4a8908b7e98a21ced2b16e9ecb5f63a
divyapuja <divyapuja.vitonde@gmail.com>
2018-11-30 13:12:41 -0500

    improved

--- roles/basicSetup/tasks/installation.sh
+++ roles/basicSetup/tasks/installation.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-sudo docker exec -it LC1 apt-get update
-sudo docker exec -it LC1 apt-get upgrade -y
-sudo docker exec -it LC1 apt-get install net-tools
-sudo docker exec -it LC1 apt-get install iproute2 -y
-sudo docker exec -it LC1 apt install iputils-ping -y
-sudo docker exec -it LC2 apt-get update
-sudo docker exec -it LC2 apt-get upgrade -y
-sudo docker exec -it LC2 apt-get install net-tools
-sudo docker exec -it LC2 apt-get install iproute2 -y
-sudo docker exec -it LC2 apt install iputils-ping -y
-sudo docker exec -it SC1 apt-get update
-sudo docker exec -it SC1 apt-get upgrade -y
-sudo docker exec -it SC1 apt-get install net-tools
-sudo docker exec -it SC1 apt-get install iproute2 -y
-sudo docker exec -it SC1 apt install iputils-ping -y
-sudo docker exec -it SC2 apt-get update
-sudo docker exec -it SC2 apt-get upgrade -y
-sudo docker exec -it SC2 apt-get install net-tools
-sudo docker exec -it SC2 apt-get install iproute2 -y
-sudo docker exec -it SC2 apt install iputils-ping -y
+sudo docker exec -i LC1 apt-get update
+sudo docker exec -i LC1 apt-get upgrade -y
+sudo docker exec -i LC1 apt-get install net-tools
+sudo docker exec -i LC1 apt-get install iproute2 -y
+sudo docker exec -i LC1 apt install iputils-ping -y
+sudo docker exec -i LC2 apt-get update
+sudo docker exec -i LC2 apt-get upgrade -y
+sudo docker exec -i LC2 apt-get install net-tools
+sudo docker exec -i LC2 apt-get install iproute2 -y
+sudo docker exec -i LC2 apt install iputils-ping -y
+sudo docker exec -i SC1 apt-get update
+sudo docker exec -i SC1 apt-get upgrade -y
+sudo docker exec -i SC1 apt-get install net-tools
+sudo docker exec -i SC1 apt-get install iproute2 -y
+sudo docker exec -i SC1 apt install iputils-ping -y
+sudo docker exec -i SC2 apt-get update
+sudo docker exec -i SC2 apt-get upgrade -y
+sudo docker exec -i SC2 apt-get install net-tools
+sudo docker exec -i SC2 apt-get install iproute2 -y
+sudo docker exec -i SC2 apt install iputils-ping -y
