72840766fbb9aa63e9a5bb4973336a11289a71bd
divyapuja <divyapuja.vitonde@gmail.com>
2018-11-29 21:29:54 -0500

    basic setup script

--- initial.sh
+++ initial.sh
@@ -3,3 +3,4 @@
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install -y ansible
+sudo apt install -y docker.io
---
+++ roles/basicSetup/tasks/installation.sh
@@ -0,0 +1,20 @@
+sudo docker exec -it LC1 apt-get update
+sudo docker exec -it LC1 apt-get upgrade -y
+sudo docker exec -it LC1 apt-get install net-tools
+sudo docker exec -it LC1 apt-get install iproute2 -y
+sudo docker exec -it LC1 apt install iputils-ping -y
+sudo docker exec -it LC2 apt-get update
+sudo docker exec -it LC2 apt-get upgrade -y
+sudo docker exec -it LC2 apt-get install net-tools
+sudo docker exec -it LC2 apt-get install iproute2 -y
+sudo docker exec -it LC2 apt install iputils-ping -y
+sudo docker exec -it SC1 apt-get update
+sudo docker exec -it SC1 apt-get upgrade -y
+sudo docker exec -it SC1 apt-get install net-tools
+sudo docker exec -it SC1 apt-get install iproute2 -y
+sudo docker exec -it SC1 apt install iputils-ping -y
+sudo docker exec -it SC2 apt-get update
+sudo docker exec -it SC2 apt-get upgrade -y
+sudo docker exec -it SC2 apt-get install net-tools
+sudo docker exec -it SC2 apt-get install iproute2 -y
+sudo docker exec -it SC2 apt install iputils-ping -y
